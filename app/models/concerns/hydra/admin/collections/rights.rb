52c44cd353c215374de6be9fc4bbee25b55f6d7a
Sarah <reid.419@osu.edu>
2015-02-03 14:45:09 -0500

    Changed 'type's to 'predicate's.

--- app/models/concerns/hydra/admin/collections/rights.rb
+++ app/models/concerns/hydra/admin/collections/rights.rb
@@ -3,6 +3,6 @@ module Hydra::Admin::Collections::Rights
   include Hydra::AccessControls::Permissions
   included do
     # Default rights get copied onto new members.  This is an access template.
-    contains 'defaultRights', type: Hydra::Admin::Collections::NonIndexedRights, autocreate: true
+    contains 'defaultRights', predicate: Hydra::Admin::Collections::NonIndexedRights, autocreate: true
   end
 end
