3759ac8ca66b0df99b422576569d1a29276af142
Sarah <reid.419@osu.edu>
2015-02-04 12:52:01 -0500

    I'm convinced we do not need this extension. It is deprecated.

--- app/models/concerns/hydra/admin/collections/rights.rb
+++ app/models/concerns/hydra/admin/collections/rights.rb
@@ -1,8 +1,4 @@
 module Hydra::Admin::Collections::Rights
   extend ActiveSupport::Concern
   include Hydra::AccessControls::Permissions
-  included do
-    # Default rights get copied onto new members.  This is an access template.
-    contains 'defaultRights', predicate: Hydra::Admin::Collections::NonIndexedRights, autocreate: true
-  end
 end
