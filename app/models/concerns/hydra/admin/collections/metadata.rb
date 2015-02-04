85d4d572927d4a4c533f047fffc72669b6ecf711
Sarah <reid.419@osu.edu>
2015-02-04 10:39:15 -0500

    Change the way the metadata is described.

--- app/models/concerns/hydra/admin/collections/metadata.rb
+++ app/models/concerns/hydra/admin/collections/metadata.rb
@@ -1,18 +1,38 @@
 module Hydra::Admin::Collections::Metadata
   extend ActiveSupport::Concern
   included do
-    contains 'descMetadata', predicate: ActiveFedora::SimpleDatastream do |sds|
-      sds.field :name, :string
-      sds.field :description, :string
-      sds.field :publisher, :string
-      sds.field :type, :string
-      sds.field :language, :string
-      sds.field :coverage, :string
-      sds.field :creator, :string
+     # In place of :name?
+    property :name, predicate: RDF::DC.name, multiple: false do |index|
+      index.as :stored_searchable
     end
+    property :description, predicate: RDF::DC.description, multiple: false do |index|
+      index.type :text
+      index.as :stored_searchable
+    end
+    property :publisher, predicate: RDF::DC.publisher do |index|
+      index.as :stored_searchable, :facetable
+    end
+    property :language, predicate: RDF::DC.language do |index|
+      index.as :stored_searchable, :facetable
+    end
+    property :creator, predicate: RDF::DC.creator do |index|
+      index.as :stored_searchable, :facetable
+    end
+   
+    
 
-    has_attributes :description, :name, :publisher, :type, :language, :coverage, datastream: :descMetadata, multiple: false
-    has_attributes :creator, datastream: :descMetadata, multiple: true
+    # contains 'descMetadata', predicate: ActiveFedora::SimpleDatastream do |sds|
+    #   sds.field :name, :string
+    #   sds.field :description, :string
+    #   sds.field :publisher, :string
+    #   sds.field :type, :string
+    #   sds.field :language, :string
+    #   sds.field :coverage, :string
+    #   sds.field :creator, :string
+    # end
+
+    # has_attributes :description, :name, :publisher, :type, :language, :coverage, datastream: :descMetadata, multiple: false
+    # has_attributes :creator, datastream: :descMetadata, multiple: true
   end
 end
 
