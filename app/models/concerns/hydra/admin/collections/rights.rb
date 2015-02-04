ece5c67ff2547861c21783439b6c280353469d5b
Sarah <reid.419@osu.edu>
2015-02-04 12:34:09 -0500

    Maybe I should keep this...

--- app/models/concerns/hydra/admin/collections/rights.rb
+++ app/models/concerns/hydra/admin/collections/rights.rb
@@ -1,8 +1,8 @@
 module Hydra::Admin::Collections::Rights
   extend ActiveSupport::Concern
   include Hydra::AccessControls::Permissions
-  # included do
+  included do
     # Default rights get copied onto new members.  This is an access template.
-    # contains 'defaultRights', predicate: Hydra::Admin::Collections::NonIndexedRights, autocreate: true
-  # end
+    contains 'defaultRights', predicate: Hydra::Admin::Collections::NonIndexedRights, autocreate: true
+  end
 end
